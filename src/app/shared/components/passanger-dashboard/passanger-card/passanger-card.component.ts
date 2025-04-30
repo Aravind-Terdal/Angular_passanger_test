1fcf5d4c53677f2a14884aee3f178a8c68821bfb
Aravind Terdal <aravindterdal@gmail.com>
2025-04-30 16:03:18 +0530

    miner changer

--- src/app/shared/components/passanger-dashboard/passanger-card/passanger-card.component.ts
+++ src/app/shared/components/passanger-dashboard/passanger-card/passanger-card.component.ts
@@ -7,30 +7,34 @@ import { SnackBarService } from 'src/app/shared/services/snack-bar.service';
 @Component({
   selector: 'app-passanger-card',
   templateUrl: './passanger-card.component.html',
-  styleUrls: ['./passanger-card.component.scss']
+  styleUrls: ['./passanger-card.component.scss'],
 })
 export class PassangerCardComponent implements OnInit {
-  @Input() passObj ! : Ipassanger
-  isInEditMode : boolean = false
+  @Input() passObj!: Ipassanger;
+  isInEditMode: boolean = false;
   constructor(
-    private _passangerService : PassangerService,
-    private openSnackBAr : SnackBarService
-  ) { }
+    private _passangerService: PassangerService,
+    private openSnackBAr: SnackBarService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onPassNameUpdate(updatedName : string){
-    if(this.isInEditMode){
-      let updatedPassObj : Ipassanger = {...this.passObj, fullname : updatedName}
-      this._passangerService.updateNameofPassanger(updatedPassObj)
-      this.openSnackBAr.openSnackBar(`Passanger Name ${updatedName} Updated`)
+  onPassNameUpdate(updatedName: string) {
+    if (this.isInEditMode) {
+      let updatedPassObj: Ipassanger = {
+        ...this.passObj,
+        fullname: updatedName,
+      };
+      this._passangerService.updateNameofPassanger(updatedPassObj);
+      this.openSnackBAr.openSnackBar(`Passanger Name ${updatedName} Updated`);
     }
     this.isInEditMode = !this.isInEditMode;
   }
 
-  onRemove(){
+  onRemove() {
     this._passangerService.removePass(this.passObj.id);
-    this.openSnackBAr.openSnackBar(`Passanger ${this.passObj.fullname} Removed Successfully`)
+    this.openSnackBAr.openSnackBar(
+      `Passanger ${this.passObj.fullname} Removed Successfully`
+    );
   }
 }
